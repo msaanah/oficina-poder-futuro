a8431e3048eeb937fa38fe956b0567e5739177af
Ana Paula <msanapfs@gmail.com>
2023-01-21 15:58:26 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+alert ("Olá mundo!")
+prompt ("qual o seu nome");
+var primeiro = prompt ("digite seu nome")
+console.log(primeiro);
+/* Primeiro código apoós dar um google*/
+/* 1) pede duas vezes o nome da pessoa e mostra somente a última porque vc 
+pede para guardar somente na linha que tem o var*/
+/* 2) lembre-se de desativar o live view quando entrar ou se continuar
+mostrando a mesma linha e nao atualizar*/
+/* no js não é necessário usar sempre o ; Veja a terceira linha*/
+/* os comandos devem ser digitados em letras minusculas. O js faz 
+distinção entre maiuscula e minuscula */
+
+/* compartilhar no GitHub */ 
+/* abra o cmd e digite git ou abra o git bash - melhor
+abir esse por enquanto 
+subir para o git:
+git config --global user.name "Ana Paula"
+git config --global user.email digite o email sem aspas
+
+CRIAR RESPOSITORIO NO GIT
+vai para o <GIT></GIT>
+*/
+
+
+
